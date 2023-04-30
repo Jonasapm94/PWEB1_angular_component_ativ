c34a7e34fa6232dd849b820f438b0b107332118b
Jonas A <jonasapm94@gmail.com>
2023-04-30 14:11:09 -0300

    finalizado atividade

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'PWEB1_angular_component_ativ'`, () => {
+  it(`should have as title 'social'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('PWEB1_angular_component_ativ');
+    expect(app.titulo).toEqual('social');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('PWEB1_angular_component_ativ app is running!');
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('.content span').textContent).toContain('social app is running!');
   });
 });
